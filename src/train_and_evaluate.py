49692f1ebe8b2c1f4aa89c7a230cbc90c6192c0c
ssr-1998 <shubhamsinghrana9@gmail.com>
2021-06-25 15:44:52 +0530

    tracker added

--- src/train_and_evaluate.py
+++ src/train_and_evaluate.py
@@ -50,7 +50,7 @@ def train_and_evaluate(config_path):
     predicted_qualities = lr.predict(test_x)
     (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-    print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+    print("Elasticnet model (alpha={}, l1_ratio={}):".format(alpha, l1_ratio))
     print("  RMSE: %s" % rmse)
     print("  MAE: %s" % mae)
     print("  R2: %s" % r2)
@@ -78,7 +78,7 @@ def train_and_evaluate(config_path):
     joblib.dump(lr, model_path)
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     arg = argparse.ArgumentParser()
     arg.add_argument("--config", default="params.yaml")
     arg_parser = arg.parse_args()
